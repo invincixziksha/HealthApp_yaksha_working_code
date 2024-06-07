b2e5a1401360f9910c8b83430b81b584766c8c62
invincixziksha <incixziksha@gmail.com>
2024-06-07 13:14:20 +0530

    intial commit of 6 usecases

---
+++ src/main/java/pages/dynamicReport_Pages.java
@@ -0,0 +1,35 @@
+package pages;
+
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class dynamicReport_Pages extends StartupPage {
+
+	By usernameTextfield=By.id("username_id");
+	
+	String pageName = this.getClass().getSimpleName();
+	public dynamicReport_Pages(WebDriver driver) {
+		super(driver);
+	}
+	
+	/**@Test1
+	 * about this method loginToTheYakshahealthApp()
+	 * @param : Map<String, String>
+	 * @description : login to the yakshaHealth app by providing valid user name and password
+	 * @return : boolean
+	 * @author : Yaksha
+	 */
+	public boolean loginToTheYakshahealthApp(Map<String, String> loginData) throws Exception {
+		boolean isDisplayed=false;
+		try {
+			
+				isDisplayed=true;
+			
+		}catch(Exception e) {
+			throw e;
+		}
+		return isDisplayed;
+	}
+}
---
+++ src/test/java/testcases/dynamicReport_testcase.java
@@ -0,0 +1,68 @@
+package testcases;
+
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import coreUtilities.testutils.ApiHelper;
+import coreUtilities.utils.FileOperations;
+import pages.StartupPage;
+import pages.yakshaHealthApp_L1_Pages;
+import testBase.AppTestBase;
+import testdata.LocatorsFactory;
+
+public class dynamicReport_testcase extends AppTestBase
+{
+	Map<String, String> configData;
+	Map<String, String> loginCredentials;
+	String expectedDataFilePath = testDataFilePath + "expected_data.json";
+	String loginFilePath = loginDataFilePath + "Login.json";
+	StartupPage startupPage;
+	yakshaHealthApp_L1_Pages yakshaHealthAppL1Instance;
+	LocatorsFactory locatorsFactoryInstance;
+
+
+	@Parameters({"browser", "environment"})
+	@BeforeClass(alwaysRun = true)
+	public void initBrowser(String browser, String environment) throws Exception {
+		configData = new FileOperations().readJson(config_filePath, environment);
+		configData.put("url", configData.get("url").replaceAll("[\\\\]", ""));
+		configData.put("browser", browser);
+
+		boolean isValidUrl = new ApiHelper().isValidUrl(configData.get("url"));
+		Assert.assertTrue(isValidUrl, configData.get("url")+" might be Server down at this moment. Please try after sometime.");
+		initialize(configData);
+		startupPage = new StartupPage(driver);
+	}
+
+	@Test(priority = 1, groups = {"sanity"}, description="Verify the title and url of  the current page.")
+	public void verifyTitleOfTheHomePage() throws Exception {
+
+		yakshaHealthAppL1Instance = new yakshaHealthApp_L1_Pages(driver);
+		locatorsFactoryInstance = new LocatorsFactory(driver);
+
+		Map<String, String> loginData = new FileOperations().readJson(loginFilePath, "credentials");
+		Assert.assertTrue(yakshaHealthAppL1Instance.loginToHealthAppByGivenValidCredetial(loginData),"Login failed, Invalid credentials ! Please check manually");
+
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "healthApp");
+		Assert.assertEquals(yakshaHealthAppL1Instance.verifyTitleOfThePage(),expectedData.get("dasboardTitle")) ;
+		Assert.assertEquals(yakshaHealthAppL1Instance.verifyURLOfThePage(),expectedData.get("pageUrl")) ;
+		Assert.assertTrue(locatorsFactoryInstance.totalDoctorTextIsPresent(driver).isDisplayed(), "total doctors text is not present in the current page, Please check manually");
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void tearDown() {
+		System.out.println("before closing the browser");
+		browserTearDown();
+	}
+
+	@AfterMethod
+	public void retryIfTestFails() throws Exception {
+		startupPage.navigateToUrl(configData.get("url"));
+	}
+}
